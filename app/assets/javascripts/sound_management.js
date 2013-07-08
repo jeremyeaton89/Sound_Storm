35f6ccdd97765a479b0163e9d6ee080730209826
Jeremy Eaton <jeremyeaton89@gmail.com>
2013-07-08 12:15:12 -0700

    add audio seek, pause and play

--- app/assets/javascripts/sound_management.js
+++ app/assets/javascripts/sound_management.js
@@ -1,16 +1,59 @@
 $(function() {
-	
-	$("html").on("click", ".play-area", function() {
+	var interval = window.setInterval();
+	$("body").on("click", ".seeker-container", function(event) {
+		var $thisAudio = $(this).closest(".widget").find("audio");
 		// debugger
-		$(this).closest(".widget").toggleClass("playing");
+		$thisAudio[0].currentTime = (event.offsetX - 2)/ $(this).width()  * $thisAudio[0].duration;
+		console.log(event);
+		// debugger
+	});
 
-		if ($(this).closest(".widget").hasClass("playing")) {
-			$(this).find("audio")[0].play();
-			$("audio").not($(this).find("audio"))[0].pause()
-			$(".sound").not(this).removeClass("playing");
+	// on timeupdate, do below
+	
+	$("body").on("click", ".play-button", function() {
+		var $thisAudio = $(this).parent().siblings("audio");
+		// var interval ; 
+
+		if ($(this).hasClass("play")) {
+			interval = window.clearInterval(interval);
+			// if (interval !== "undefined") interval = window.clearInterval(interval);
+			// pause all audio
+			$(".play-button").addClass("play").removeClass("pause");
+			$("audio").each(function(i, audio) { audio.pause() });
+			// play this audio
+			$(this).removeClass("play").addClass("pause")
+			$thisAudio[0].play();
+
+			var that = this;
+			interval = window.setInterval(function() {
+				var width = $thisAudio[0].currentTime / $thisAudio[0].duration * 100;
+				$(that).parent().siblings(".visualizer").find(".seeker").width(width + "%");		
+			}, 200);
+
 		} else {
-			$(this).find("audio")[0].pause();
+			$(this).addClass("play").removeClass("pause");
+			$thisAudio[0].pause();
+			interval = window.clearInterval(interval);
 		}
+
+	});
+
+
+
+	// $("body").on("click", ".play-button", function() {
+	// 	// debugger
+	// 	$(this).closest(".widget").toggleClass("playing");
+
+	// 	if ($(this).closest(".widget").hasClass("playing")) {
+	// 		$(this).removeClass("play").addClass("pause").find("audio")[0].play();
+	// 		$(".play-button").not($(this)).removeClass("play").addClass("pause")
+	// 		$("audio").not($(this).find("audio"))[0].pause()
+	// 		$(".sound").not(this).removeClass("playing");
+	// 	} else {
+	// 		$(this).find("audio")[0].pause();
+	// 	}
+
+
 		// if ($(this).closest(".widget").hasClass("playing")) {
 		// 	$(this).find("audio")[0].pause();
 		// 	$(this).closest(".widget").removeClass("playing");
@@ -22,7 +65,7 @@ $(function() {
 		// }
 
 		// $("audio").not(this).each(function(i, audio) { audio.pause() });
-	});
+	// });
 
 
 });	
